b1f160b447b7a5daed9848f71626db35b4675e7d
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2023-03-23 19:34:31 +0530

    first

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-const connectDB = require("./db/connect");
 require("dotenv").config();
+const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/errorr-handler");
 
